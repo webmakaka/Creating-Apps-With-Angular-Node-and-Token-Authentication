3f45cd6c62d5c90db602ab61b8f009b32bb84ee8
marley-nodejs <marley.nodejs@gmail.com>
2015-02-01 05:20:49 +0000

    07. Auth Provider | 54-Window Popup

--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -20,9 +20,16 @@ angular.module('creatingAppsWithAngularNodeAndTokenAuthenticationApp')
           }).success(authSuccessful);
       }
 
+      var urlBuilder = [];
+
+      urlBuilder.push('response_type=code',
+                      'client_id=710007462115-pn3o6o926mr9os2t8d16tq91u2q3vdml.apps.googleusercontent.com',
+                      'redirect_uri=' + window.location.origin,
+                      'scope=profile email')
+
       this.googleAuth = function(){
 
-          var url = "https://accounts.google.com/o/oauth2/auth";
+          var url = "https://accounts.google.com/o/oauth2/auth?" + urlBuilder.join('&');
           var options = "width=500, height=500, left=" + ($window.outerWidth - 500) / 2 + ", top=" + ($window.outerWidth - 500) / 2.5;
 
           $window.open(url, '', options);
